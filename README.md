5929cc82d4dd18c42d68b30c70c67134a9f2fc06
Giorgio Garofalo <giogar2705@gmail.com>
2020-06-06 20:19:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BotMaker
+BotMaker is a work-in-progress software that allows creating Telegram bots through a user-friendly interface.   
+Developed by [iAmGio](https://github.com/iAmGio) and [AgeOfWar](https://github.com/AgeOfWar).
---
+++ src/main/kotlin/eu/iamgio/botmaker/BotMaker.kt
@@ -0,0 +1,18 @@
+package eu.iamgio.botmaker
+
+import javafx.application.Application
+import javafx.stage.Stage
+
+/**
+ * @author Giorgio Garofalo
+ */
+class BotMaker : Application() {
+
+    override fun start(primaryStage: Stage?) {
+
+    }
+}
+
+fun main() {
+    Application.launch(BotMaker::class.java)
+}
