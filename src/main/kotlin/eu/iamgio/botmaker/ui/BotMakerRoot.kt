b1cf5122f245997284d30df258a43b0344dd82c3
Giorgio Garofalo <giogar2705@gmail.com>
2020-06-06 23:03:57 +0100

    Implement nav-bar(s)

--- src/main/kotlin/eu/iamgio/botmaker/ui/BotMakerRoot.kt
+++ src/main/kotlin/eu/iamgio/botmaker/ui/BotMakerRoot.kt
@@ -2,28 +2,27 @@ package eu.iamgio.botmaker.ui
 
 import javafx.scene.control.SplitPane
 import javafx.scene.layout.AnchorPane
-import javafx.scene.layout.VBox
 
 /**
  * @author Giorgio Garofalo
  */
 class BotMakerRoot : AnchorPane() {
 
-    private val leftControl = VBox().apply {
-        SplitPane.setResizableWithParent(this, false)
+    private val splitPane = SplitPane().apply {
+        bindSize(this@BotMakerRoot)
+        setDividerPositions(0.25)
+    }
+
+    private val leftControl = SplitControl().apply {
         styleClass += "left-control"
     }
 
-    private val rightControl = VBox().apply {
-        SplitPane.setResizableWithParent(this, false)
+    private val rightControl = SplitControl().apply {
         styleClass += "right-control"
     }
 
     init {
-        val splitPane = SplitPane(leftControl, rightControl).apply {
-            bindSize(this@BotMakerRoot)
-            setDividerPositions(0.25)
-        }
+        splitPane.items.addAll(leftControl, rightControl)
         children += splitPane
     }
 }
---
+++ src/main/kotlin/eu/iamgio/botmaker/ui/SplitControl.kt
@@ -0,0 +1,25 @@
+package eu.iamgio.botmaker.ui
+
+import javafx.scene.control.SplitPane
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
+
+private const val NAVBAR_HEIGHT = 50.0
+
+/**
+ * @author Giorgio Garofalo
+ */
+class SplitControl : VBox() {
+
+    private val navBar = HBox().apply {
+        styleClass += "nav-bar"
+        bindSize(this@SplitControl, bindHeight = false)
+        prefHeight = NAVBAR_HEIGHT
+    }
+
+    init {
+        styleClass += "split-control"
+        SplitPane.setResizableWithParent(this, false)
+        children += navBar
+    }
+}
