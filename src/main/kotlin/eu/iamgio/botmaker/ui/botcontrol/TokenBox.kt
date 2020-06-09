5b9cdba278ba01d5b70703115031e7844fa2b8e8
Giorgio Garofalo <giogar2705@gmail.com>
2020-06-09 18:07:58 +0100

    Make token editable

--- src/main/kotlin/eu/iamgio/botmaker/ui/botcontrol/TokenBox.kt
+++ src/main/kotlin/eu/iamgio/botmaker/ui/botcontrol/TokenBox.kt
@@ -2,9 +2,13 @@ package eu.iamgio.botmaker.ui.botcontrol
 
 import eu.iamgio.botmaker.bundle.getString
 import eu.iamgio.botmaker.lib.BotConfiguration
+import eu.iamgio.botmaker.root
 import eu.iamgio.botmaker.ui.*
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.geometry.Pos
 import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.scene.input.KeyCode
 import javafx.scene.layout.HBox
 
 /**
@@ -17,21 +21,79 @@ class TokenBox(bot: BotConfiguration) : HBox() {
         styleClass += "token-box"
         children += Label(getString("token")).withClass("token")
 
-        val tokenLabel = Label(bot.botToken).withClass("token-label")
+        val editableToken = EditableToken(bot)
 
+        // Visibility on/off button
         children += createSvg().apply {
-            tokenLabel.visibleProperty().addListener { _, _, visible ->
+            editableToken.visibleProperty().addListener { _, _, visible ->
                 content = if(visible) SVG_VISIBILITY_OFF else SVG_VISIBILITY_ON
             }
-            tokenLabel.isVisible = false
+            editableToken.isVisible = false
         }.wrap().apply {
             setOnMouseClicked {
-                tokenLabel.isVisible = !tokenLabel.isVisible
+                editableToken.isVisible = !editableToken.isVisible
+                if(editableToken.isEditing) editableToken.toggle()
             }
         }
 
-        children += createSvg(SVG_EDIT).wrap() // TODO token edit
+        // Edit button
+        children += createSvg(SVG_EDIT).wrap().apply {
+            disableProperty().bind(editableToken.visibleProperty().not())
+            visibleProperty().bind(disableProperty().not())
+            setOnMouseClicked {
+                editableToken.toggle()
+            }
+        }
 
-        children += tokenLabel
+        children += editableToken
+    }
+}
+
+private class EditableToken(bot: BotConfiguration) : HBox() {
+
+    private val label = Label(bot.botToken).withClass("token-label").apply { isWrapText = true }
+    private val textField = TextField()
+
+    val editingProperty = SimpleBooleanProperty()
+    val isEditing: Boolean
+        get() = editingProperty.value
+
+    init {
+        alignment = Pos.CENTER_LEFT
+
+        editingProperty.addListener { _ ->
+            if(isEditing) {
+                textField.text = label.text
+                children.setAll(textField)
+            } else {
+                children.setAll(label)
+            }
+        }
+
+        editingProperty.bind(label.visibleProperty().not())
+
+        textField.visibleProperty().bind(editingProperty)
+        textField.disableProperty().bind(editingProperty.not())
+        label.disableProperty().bind(editingProperty)
+
+        label.prefWidthProperty().bind(root.prefWidthProperty().divide(5))
+
+        textField.setOnAction {
+            label.text = textField.text
+            toggle()
+            // TODO change token
+        }
+        textField.setOnKeyReleased {
+            if(it.code == KeyCode.ESCAPE) toggle()
+        }
+    }
+
+    fun toggle() {
+        label.isVisible = !label.isVisible
+
+        if(isEditing) {
+            textField.requestFocus()
+            textField.requestFocus()
+        }
     }
 }
