be7cb88129761b64167c52367b7270125c0117fb
Giorgio Garofalo <giogar2705@gmail.com>
2020-06-13 13:07:31 +0100

    Implement event actions (UI only)

--- src/main/kotlin/eu/iamgio/botmaker/ui/botcontrol/event/EventNode.kt
+++ src/main/kotlin/eu/iamgio/botmaker/ui/botcontrol/event/EventNode.kt
@@ -11,9 +11,26 @@ import javafx.scene.layout.VBox
  */
 class EventNode(event: Event<*>) : VBox() {
 
+    private val actionsVBox = VBox().withClass("actions")
+
     init {
         styleClass += "event"
 
-        children += Label(getString("event.name." + event.javaClass.simpleName)).withClass("event-title")
+        children += Label(getString("event.name." + event.javaClass.simpleName) + ":").withClass("event-title")
+
+        children += actionsVBox
+
+        actionsVBox.children += Label("+ ${getString("new.action")}").withClass("new").apply {
+            setOnMouseClicked {
+                println("New action")
+                // TODO new action
+            }
+        }
+
+        addAction("Test bla bla bla")
+    }
+
+    fun addAction(text: String) {
+        actionsVBox.children.add(actionsVBox.children.size - 1, Label(text).withClass("event-action"))
     }
 }
